4116d74b41115e1a4bf4b8eaa5308423256e81e2
LowC0ff <henriquelima.dev@gmail.com>
2022-04-20 23:58:36 -0300

    Start

--- main/main.js
+++ main/main.js
@@ -8,13 +8,7 @@ bot.start(ctx => {
     const firstName = ctx.update.message.from.first_name
     const lastName = ctx.update.message.from.last_name
 
-    let idAcess = env.idList.filter(from.id)
-
-    if (from.id == idAcess) {
-        ctx.reply('Access denied. Contact my master')
-        console.log(from)
-    }
-    else ctx.reply(`Hello,${firstName} ${lastName}! Please select language`)
+    ctx.reply(`Hello,${firstName} ${lastName}!`)
 })
 
 bot.hears(/github/i, ctx => ctx.reply('https://github.com/LowC0ff'))
