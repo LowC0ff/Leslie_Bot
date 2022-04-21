5b7f306597a347d3c3297e804555e2c7dbe66d5a
LowC0ff <henriquelima.dev@gmail.com>
2022-04-21 20:28:33 -0300

    keyboards

--- main/main.js
+++ main/main.js
@@ -8,8 +8,11 @@ bot.start(ctx => {
     const firstName = ctx.update.message.from.first_name
     const lastName = ctx.update.message.from.last_name
 
-    ctx.reply(`Hello,${firstName} ${lastName}!`)
+    ctx.reply(`Hello, ${firstName} ${lastName}!`)
 })
+
+bot.hears(/lillyan/i, ctx => ctx.reply('eu lhe avisei que ela não é de confiança...'))
+bot.hears(/conselho/i, ctx => ctx.reply('foi bom porque agora você vai ter a prova que ela não é confiável'))
 
 bot.hears(/github/i, ctx => ctx.reply('https://github.com/LowC0ff'))
 
--- main/teclado.js
+++ main/teclado.js
@@ -3,6 +3,24 @@ const Telegraf = require('telegraf')
 const Markup = require('telegraf/markup')
 const bot = new Telegraf(env.token)
 
-const tecladoCarne= Markup.keyboard([
-
+const tecladoTest= Markup.keyboard([
+    [bottom1],
+    [bottom2],
+    [bottom3]
 ]).resize().extra()
+
+bot.start(async ctx => {
+    await ctx.reply('qual bebida você prefere?')
+       Markup.keyboard(['coca', 'pepsi']).resize().oneTime().extra()
+})
+
+bot.hears(['coca', 'pepsi'], async ctx => {
+    await ctx.reply(`Ai sim, gosto de ${ctx.match} também`)
+    await ctx.reply('escolha um botão', tecladoTest)
+})
+
+bot.hears('bottom1', ctx => ctx.reply('você teclou no botão 1'))
+bot.hears('bottom2', ctx => ctx.reply('você teclou no botão 2'))
+bot.hears('bottom3', ctx => ctx.reply('você teclou no botão 3'))
+
+bot.startPolling()
